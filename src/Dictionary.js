e26eb59bbc9ad53dd5305b03e45a66b075811168
katiewalker126 <katiewalker126@gmail.com>
2022-04-29 16:53:07 +0100

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
@@ -16,7 +22,7 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form className="search-form text-center" onSubmit={search}>
         <input type="search" onChange={updateKeyword}></input>
-        <input type="submit" value="Search" classNam="search-button"></input>
+        <input type="submit" value="Search" className="search-button"></input>
       </form>
     </div>
   );
